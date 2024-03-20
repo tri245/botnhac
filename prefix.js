5d108024a7daf893afa97122404f57137915fae0
Tri_nho <76763671+tri245@users.noreply.github.com>
2024-03-20 11:41:42 +0700

    Update prefix.js

--- prefix.js
+++ prefix.js
@@ -1,7 +1,6 @@
 const { EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const db = require("../mongodb");
 // Function to load the prefix from the JSON file
 function loadPrefix() {
     const filePath = path.join(__dirname, '..', 'data', 'prefix.json');
